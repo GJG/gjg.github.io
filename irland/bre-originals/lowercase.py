da757579c179f47fb95541990c03afb878814dde
gjg <gjg@tskoli.is>
2025-06-04 18:18:39 +0100

    ok

--- irland/bre-originals/lowercase.py
+++
@@ -1,8 +0,0 @@
-import os
-
-folder = '.'
-for filename in os.listdir(folder):
-    if filename.endswith('.JPG'):
-        new_name = filename[:-4] + '.jpg'
-        os.rename(os.path.join(folder, filename), os.path.join(folder, new_name))
-        print(f'Renamed: {filename} -> {new_name}')
